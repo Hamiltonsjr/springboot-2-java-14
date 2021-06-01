3bc89333855accbc2f5c50787a4e479830be1a8d
Hamiltonsjr <hamiltonsjr@hotmail.com>
2021-06-01 10:28:51 -0300

    Project created

---
+++ src/test/java/com/example/projectwebservicie/ProjectWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectwebservicie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
